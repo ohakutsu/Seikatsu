8ba48a6cb9656af3091ed92091a2c566752d256e
TakuyaHanada <takuyahanada.hntk@gmail.com>
2019-11-08 14:08:28 +0900

    [add, update] Itemモデルの作成

---
+++ src/app/models/item.rb
@@ -0,0 +1,13 @@
+class Item < ApplicationRecord
+  belongs_to :user
+
+  validates :uuid, presence: true, uniqueness: true
+  validates :title, presence: true, length: {
+    minimum: 1,
+    maximum: 20
+  }
+  validates :body, presence: true, length: {
+    minimum: 1,
+    maximum: 5000
+  }
+end
