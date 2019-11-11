5d92fc2ad8bc3ccb8569e39e062e4536ba02e99a
TakuyaHanada <takuyahanada.hntk@gmail.com>
2019-11-11 12:32:38 +0900

    [fix] 微修正

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,8 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_08_024842) do
-
+ActiveRecord::Schema.define(version: 2019_11_11_030945) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +45,15 @@ ActiveRecord::Schema.define(version: 2019_11_08_024842) do
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_votes_on_item_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
   add_foreign_key "items", "users"
--- src/db/seeds/development/users.rb
+++ src/db/seeds/development/users.rb
@@ -1,7 +1,9 @@
-emails = ['hellotaro@example.com', 'mikemike@example.com', 'yesalexcan@example.com']
-passwords = ['passtaro', 'mikepass', 'alexword']
-names = ['太郎', 'Mike', 'Alex']
-descriptions = ['太郎です。よろしく。', 'I am Mike.', 'Nothing']
+# frozen_string_literal: true
+
+emails = ["hellotaro@example.com", "mikemike@example.com", "yesalexcan@example.com"]
+passwords = ["passtaro", "mikepass", "alexword"]
+names = ["\u592A\u90CE", "Mike", "Alex"]
+descriptions = ["\u592A\u90CE\u3067\u3059\u3002\u3088\u308D\u3057\u304F\u3002", "I am Mike.", "Nothing"]
 
 0.upto(2) do |i|
   User.create(
