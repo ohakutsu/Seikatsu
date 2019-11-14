29f9a1de4d74bdbd156744a3e4f95b125b71b5c8
TakuyaHanada <takuyahanada.hntk@gmail.com>
2019-11-14 22:44:41 +0900

    [add, update] itemの作成

--- src/app/controllers/items_controller.rb
+++ src/app/controllers/items_controller.rb
@@ -10,17 +10,39 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new()
   end
 
   def edit
+    @item = Item.find_by_hashid(params[:id])
   end
 
   def create
+    @item = Item.new(params.require(:item).permit(:title, :body, :user_id))
+    @item.user_id = 1
+
+    if @item.save
+      redirect_to @item, notice: "投稿しました"
+    else
+      render "new"
+    end
   end
 
   def update
+    @item = Item.find_by_hashid(params[:id])
+    @item.assign_attributes(params.require(:item).permit(:title, :body, :user_id))
+    @item.user_id = 2
+
+    if @item.save
+      redirect_to @item, notice: "記事を更新しました"
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @item = Item.find_by_hashid(params[:id])
+    @item.destroy
+    redirect_to :items, notice: "記事を削除しました"
   end
 end
