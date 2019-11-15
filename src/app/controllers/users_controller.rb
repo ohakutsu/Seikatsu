02abc10fa9d696699837e39bf68babe5c0b4fa9c
TakuyaHanada <takuyahanada.hntk@gmail.com>
2019-11-15 12:07:22 +0900

    [update] ページネーションに対応

--- src/app/controllers/users_controller.rb
+++ src/app/controllers/users_controller.rb
@@ -3,6 +3,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by_hashid(params[:id])
-    @user_items = @user.items.order(created_at: :desc)
+    @user_items = @user.items.order(created_at: :desc).page(params[:page]).per(5)
   end
 end
