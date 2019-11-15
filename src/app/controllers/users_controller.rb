32f449561138dfff9c595366467aa11a9bec13d1
TakuyaHanada <takuyahanada.hntk@gmail.com>
2019-11-15 10:58:00 +0900

    [add, update] user/showの作成

--- src/app/controllers/users_controller.rb
+++ src/app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  def show
+    @user = User.find_by_hashid(params[:id])
+    @user_items = @user.items.order(created_at: :desc)
+  end
 end
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   root "home#index"
   get "latest" => "home#latest"
   resources :items
-  resources :users
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
