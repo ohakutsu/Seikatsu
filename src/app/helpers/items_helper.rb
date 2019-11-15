ff99ede61cab44cfbd596e08621db9c4e8573498
TakuyaHanada <takuyahanada.hntk@gmail.com>
2019-11-15 18:48:40 +0900

    [update] 自分以外の記事を編集できないように変更

--- src/app/helpers/items_helper.rb
+++ src/app/helpers/items_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ItemsHelper
+  def is_auther?(item)
+    if current_user.present?
+      item.user.hashid == current_user.hashid
+    else
+      false
+    end
+  end
 end
